bc71513eb86651da799ef13186583bf53fa30807
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-12-06 20:24:13 +0900

    ADD :: signin 구현

--- src/main/java/com/example/basketball/dto/Userdto.java
+++ src/main/java/com/example/basketball/dto/Userdto.java
@@ -1,17 +1,22 @@
 package com.example.basketball.dto;
 
+import com.example.basketball.entity.Account;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @ToString
 public class Userdto {
     private Long id;
     private String nickname;
+    private String name;
     private String password;
+
+    public Account toEntity() {
+        return new Account(id, nickname, name, password);
+    }
 }
--- src/main/java/com/example/basketball/service/AuthService.java
+++ src/main/java/com/example/basketball/service/AuthService.java
@@ -1,11 +1,15 @@
 package com.example.basketball.service;
 
 import com.example.basketball.dto.Userdto;
+import com.example.basketball.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    @Autowired
+    UserRepository userRepository;
     public String login(Userdto userdto) {
-        return userdto.toString();
+        return "hello";
     }
 }
